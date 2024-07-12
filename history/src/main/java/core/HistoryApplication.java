aeef2a91604243cbfee62f2b39112ebe01ba684f
Sergei Prokofev <fogg8341@gmail.com>
2024-07-12 06:38:59 +0400

    remove reduant package and imports

--- history/src/main/java/core/HistoryApplication.java
+++ history/src/main/java/core/HistoryApplication.java
@@ -1,14 +1,7 @@
 package core;
-import core.entities.User;
-import core.json.JsonUtil;
-import core.service.KafkaConsumer;
-import core.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
-
 
 @SpringBootApplication
 public class HistoryApplication {
